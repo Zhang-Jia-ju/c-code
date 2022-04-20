b8e411003bec77d778d8f052b491c51c317efbaf
Zhang-Jia-ju <1375246845@qq.com>
2022-04-21 00:54:06 +0800

    速通

---
+++ C语言实验报告册/A2.c
@@ -0,0 +1,13 @@
+//求两个数的和、差、积、商
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    a = 2.0;
+    b = 3.0;
+    printf("%f\n",a+b);//和
+    printf("%f\n",a-b);//差
+    printf("%f\n",a*b);//积
+    printf("%f\n",a/b);//商
+    return 0;
+}
--- C语言速通/A1_HelloWorld与打印.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("hello world");//不要漏写f
-    return 0;
-}
-//在C语言的所有程序中 主函数main只有一个且return 后的所有程序均不会执行
