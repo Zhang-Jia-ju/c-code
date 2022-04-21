26ef3404817948802011e958e666defc71fcf186
Zhang-Jia-ju <1375246845@qq.com>
2022-04-21 17:27:52 +0800

    第一章

---
+++ C语言实验报告册/A1.c
@@ -0,0 +1,13 @@
+//求三个整数的和___10,15,20
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    int sum=0;
+    a = 10;
+    b = 15;
+    c = 20;
+    sum = a+b+c;
+    printf("%d",sum);
+    return 0;
+}
