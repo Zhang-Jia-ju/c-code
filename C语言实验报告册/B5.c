b77e1e81dc9cf520531ec1ce8573628c31a7b11f
Zhang-Jia-ju <1375246845@qq.com>
2022-04-21 18:16:17 +0800

    撤回

--- C语言实验报告册/B5.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float c = 3.123456789;
-    printf("%f",c);
-    return 0;//输出结果从第七位被截断，四舍五入到第6位
-}
--- C语言实验报告册/C6.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int m,n;
-    m=n=0;
-    scanf("%d%d",&m,&n);
-    printf("m=%d\nn=%d",m,n);
-    return 0;
-}
